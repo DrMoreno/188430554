c6fb5c03e8e0f216df15f7423b17a3cb74669ed8
CptDrMoreno <Rodrigousqui@hotmail.com>
2019-08-02 23:25:38 -0400

    taller4

---
+++ PHP/navegadorcito/login.php
@@ -0,0 +1,49 @@
+<?php
+  session_start();
+  $error=FALSE;
+  if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if(empty($_POST["username"])){
+      $error=TRUE;
+    }
+    else{
+      $_SESSION["username"]=$_POST["username"];
+    }
+  }
+
+?>
+<form action="login.php" method="get">
+  Ingrese usuario: <input type="text" name="username">
+  <br>
+  Ingrese contraseña: <input type="text" name="password">
+  <br>
+  <input type="submit" name="go">
+</form>
+<?php
+  if(isset($_GET["go"])):
+    if($_GET["username"]==$_SESSION["username"]){
+        header("Location: http://localhost/navegadorcito/index.php");
+    }
+    if($_GET["username"]!=$_SESSION["username"]):
+?>
+      <h3>Usuario no existe, porfavor registrese</h3>
+      <form action="login.php" method="post">
+        Ingrese usuario: <input type="text" name="username">
+        <br>
+        Ingrese contraseña: <input type="text" name="password">
+        <br>
+        <input type="submit">
+      </form>
+<?php
+    endif;
+  endif;
+  if($error):
+?>
+    <h3>Usuario no existe, porfavor registrese</h3>
+    <form action="login.php" method="post">
+      Ingrese usuario: <input type="text" name="username">
+      <br>
+      Ingrese contraseña: <input type="text" name="password">
+      <br>
+      <input type="submit">
+    </form>
+<?php endif ?>
